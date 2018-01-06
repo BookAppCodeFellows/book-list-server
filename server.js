ddb937eabd627a2852ecc747e35532f57abb14a0
Rafael Malave <rafomalave@gmail.com>
2018-01-06 12:31:09 -0800

    express skeleton

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+// REVIEW: Check out all of our new arrow function syntax!
+
+const pg = require('pg');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const conString = 'postgres://localhost:5432/book_app';
+const client = new pg.Client(conString);
+
+client.connect();
+client.on('error', err => {
+ console.error(err);
+});
+
+app.get('/test', (req, res) => res.send('Hellow World'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
