5bf3edd218d2be1c5c8614cf2c0976487d7ff02b
Rafael Malave <rafomalave@gmail.com>
2018-01-06 13:20:38 -0800

    install cors

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 const app = express();
 const conString = 'postgres://localhost:5432/book_app';
 const client = new pg.Client(conString);
 
+app.use(cors());
+
 client.connect();
 client.on('error', err => {
- console.error(err);
-});
+  console.error(err);
+ });
 
 app.get('/test', (req, res) => res.send('Hellow World'));
 
